ed98e775f65b255c16e7fbdca761df7172ac2004
Jason Offenbaker <JasonOffenbaker@gmail.com>
2021-09-27 17:03:09 -0700

    added files and variables to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//player variables
+var healthPoints = 100;
+var attackPoints = 10;
+var moneyPoints = 10;
+
+//opponent variables
+var oponentHealth = 50;
+var opponentAttack = 12;
+
+//System Variables
+var isGameOver = new Boolean(false);
+
+//GameLoop
